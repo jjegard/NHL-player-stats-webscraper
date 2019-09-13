7b9a73791e2beb132f134e6787ff9915570cdefc
John Jegard <john.jegard@gmail.com>
2019-09-12 23:18:56 -0600

    loop through all pages and scrapes as intended. Code still needs to be cleaned up and commented better

--- scraper.py
+++ scraper.py
@@ -12,11 +12,22 @@ would be useful in a hockey pool and save this data in a CSV file.
 
 from bs4 import BeautifulSoup
 from selenium import webdriver
+from selenium.webdriver.support.select import Select
 import os
 import numpy as np
 import pandas as pd
+import time
 
 ###########################FUNCTIONS########################
+def createSoup(driver:webdriver)->BeautifulSoup:
+    '''create soup and return the portion of the soup that contains the table where the
+    the stats will be scraped from'''
+    
+    html = driver.page_source;
+    soup = BeautifulSoup(html, "html5lib");
+    table = soup.find("div", class_="rt-table");
+    return table;
+
 
 def getColumnIndices(table:BeautifulSoup, desiredStats:np.array)->list:
     """takes the desired stats np.array and returns a list of all the column indices for those player stats"""
@@ -34,13 +45,12 @@ def getColumnIndices(table:BeautifulSoup, desiredStats:np.array)->list:
     return colIndices;
 
 
-def getPlayerStats(table:BeautifulSoup, desiredStats:np.array)->pd.DataFrame:
+def getPlayerStatsOnCurrentPage(playerStatsDF:pd.DataFrame, table:BeautifulSoup, playerColIndices:list)->pd.DataFrame:
     """gets all the player stats from one page and puts it into a dataframe"""
     
-    playerColIndices = getColumnIndices(table, desiredStats);
+   
     rows = table.find_all("div", class_="rt-tr-group");
     
-    playerStatsDF = pd.DataFrame(columns=desiredStats);
     for row in rows:
     #for row in [rows[0], rows[1]]: #DELETE
         individualPlayers = row.find_all("div", class_="rt-td");
@@ -51,15 +61,48 @@ def getPlayerStats(table:BeautifulSoup, desiredStats:np.array)->pd.DataFrame:
         
     return playerStatsDF;
 
+
+def getPlayerStats(driver:webdriver, desiredStats:np.array)->pd.DataFrame:
+    countWhileLoop = 0; #DELETE
+    #create empty dataframe for data to be appended to
+    playerStatsDF = pd.DataFrame(columns=desiredStats);
+    
+    table = createSoup(driver);
+#    return table; #DELETE
+    playerColIndices = getColumnIndices(table, desiredStats);
+    
+    while True:
+        countWhileLoop = countWhileLoop + 1; #DELETE
+        
+        getPlayerStatsOnCurrentPage(playerStatsDF, table, playerColIndices);
+        
+        if(not driver.find_element_by_css_selector("div.-next .-btn").is_enabled()):
+            break;
+        
+        driver.find_element_by_css_selector("div.-next .-btn").click();
+        table = createSoup(driver);
+    print(countWhileLoop);
+    
+    return playerStatsDF;
+
+
+def changeReportType(driver:webdriver, reportType:str):
+    #Select report type from the drop down menu
+    reportMenu = Select(driver.find_element_by_css_selector("div.filter--report select"));
+    reportMenu.select_by_visible_text(reportType);
+    #Click Run Report to generate Goalie Summary table
+    driver.find_element_by_css_selector("div.toolbar__col.non-time-report-options button.go").click();
+
 ###############MAIN########################
 url = "http://www.nhl.com/stats/player?reportType=season&seasonFrom=20182019&seasonTo=20182019\
 &gameType=2&filter=gamesPlayed,gte,1&sort=points";
 
-desiredPlayerStats1 = np.array(["Player", "Team", "Pos", "GP", "G", "A", "P", "+/-", "PIM", "PPG", "PPP", "SHG",\
-                      "SHP", "GWG", "S"]);
-desiredPlayerStats2 = np.array(["Player", "Team", "HIT", "BLK"]);
-desiredGoalieStats = ["Player", "Team", "GP", "GS", "W", "L", "OTL", "GA", "GAA", "SA", "SV", "S%", "SO"];
-                 
+desiredPlayerStats1 = np.array(["Player", "Team", "Pos", "GP", "G", "A", "P", "+/-", "PIM", "PPG", "PPP", \
+                                "SHG", "SHP", "GWG", "S"]);
+desiredPlayerStats2 = np.array(["Player", "Team", "Hits", "BkS"]);
+desiredGoalieStats = np.array(["Player", "Team", "GP", "GS", "W", "L", "OT", "SA", "Svs", "GA","Sv%", \
+                               "GAA", "SO"]);
+reportType = ["Hits, BkS, MsS, Gvwys, Tkwys (since 1997-98)", "Goalie Summary"];
 
 dir_path = os.path.dirname(os.path.realpath(__file__));
 chromedriver = f'{dir_path}/chromedriver';
@@ -68,18 +111,17 @@ os.environ["webdriver.chrome.driver"] = chromedriver;
 
 driver = webdriver.Chrome(executable_path=chromedriver);
 driver.get(url);
-html = driver.page_source;
 
-soup = BeautifulSoup(html, "html5lib");
-table = soup.find("div", class_="rt-table");
-
-playerStatsDF = getPlayerStats(table, desiredPlayerStats1);
-
-driver.find_element_by_css_selector("div.-next .-btn").click();
-#html = driver.page_source;
-#soup = BeautifulSoup(html, "html5lib");
-#table = soup.find("div", class_="rt-table");
-#playerStatsDF = getPlayerStats(table, desiredPlayerStats1);
-
-
+playerStatsDF1 = getPlayerStats(driver, desiredPlayerStats1);
+changeReportType(driver, reportType[0]);
+time.sleep(1);#Allows time for the javascript to run before grabbing the html
+playerStatsDF2 = getPlayerStats(driver, desiredPlayerStats2);
+changeReportType(driver, reportType[1]);
+time.sleep(1);#Allows time for the javascript to run before grabbing the html
+goalieStatsDF = getPlayerStats(driver, desiredGoalieStats);
 driver.close();
+
+playerStatsDF = pd.merge(playerStatsDF1, playerStatsDF2, on=[desiredPlayerStats1[0], desiredPlayerStats1[1]]);
+
+playerStatsDF.to_csv("playerStats.csv", index=False);
+goalieStatsDF.to_csv("goalieStats.csv", index=False);
